0db2c1070785b6741259183561978a903287adf2
fkamag <fabiokamagian@gmail.com>
2022-09-26 19:00:02 -0300

    requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  return User;
+};
