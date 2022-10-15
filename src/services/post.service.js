acc6a9e5729ae0f52dce7f968d1fbd2e5cc48791
fkamag <fabiokamagian@gmail.com>
2022-10-15 17:41:48 -0300

    lint corrigido

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,28 +1,15 @@
-const Sequelize = require('sequelize');
-const config = require('../config/config');
-
-const env = process.env.NODE_ENV;
-const sequelize = new Sequelize(config[env]);
 const { BlogPost, PostCategory } = require('../models');
 
 const createPost = async ({ title, content, categoryIds, userId }) => {
-  // const t = await sequelize.transaction();
-  // try {
-    const newPost = await BlogPost.create(
-      { title, content, userId },
-    );
-    console.log('Posto Cadastrado com sucesso:', newPost);
-    await Promise.all(categoryIds.map(
-      (categoryId) => PostCategory.create(
-        { postId: newPost.dataValues.id, categoryId },
-      ),
-    ));
-    return newPost;
-  // } catch (err) {
-  //   console.error(err);
-  //   await t.rollback();
-  //   return err;
-  // }
+  const newPost = await BlogPost.create(
+    { title, content, userId },
+  );
+  await Promise.all(categoryIds.map(
+    (categoryId) => PostCategory.create(
+      { postId: newPost.dataValues.id, categoryId },
+    ),
+  ));
+  return newPost.dataValues;
 };
 
 module.exports = {
